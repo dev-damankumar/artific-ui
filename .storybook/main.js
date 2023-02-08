e0ad30da1a1a84d09e32539a19c8eed7b92fc634
Daman <damankumar862@gmail.com>
2023-02-08 14:23:54 +0530

    storybook version fixed

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,19 @@
 const getStories=() => {
-	return process.argv[4]==='-stories'
-		? [`../${process.argv[5]}`]
-		: [
-			'../stories/**/*.stories.@(js|mdx|ts|tsx)',
-		];
+  return process.argv[4]==='-stories'
+      ? [`../${process.argv[5]}`]
+      : [
+        '../stories/**/*.stories.@(js|mdx|ts|tsx)',
+      ];
 };
 module.exports={
-	"stories": () => getStories(),
-	"core": {
-		builder: '@storybook/builder-webpack5',
-	},
-	"addons": [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions"
-	],
-	"framework": "@storybook/react",
+  "stories": () => getStories(),
+  "core": {
+    builder: '@storybook/builder-webpack5',
+  },
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
 }
