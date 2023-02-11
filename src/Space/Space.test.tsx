2268a3de897cc240e41eb87cafb58a997360da9d
Daman <damankumar862@gmail.com>
2023-02-11 16:01:23 +0530

    changes

--- src/Space/Space.test.tsx
+++ src/Space/Space.test.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {getByText, render} from "@testing-library/react";
 import Space from "./Space";
 import {Axis, AxisArray} from "../types/Common.types";
-import {ColArray} from "../Col";
 
 describe("Render Col", () => {
 	test("render the Col component", () => {
@@ -18,23 +17,32 @@ describe("Render Col", () => {
 
 
 describe("Render Col Number", () => {
-	const spaces = [...ColArray]
+	const spaces = [5, 10, 15, 20]
 	spaces.forEach(space => {
 		test(`render ${space} space`, () => {
 			const {container} = render(<Space gap={space as number}>Click Me</Space>);
 			const component = container.firstChild;
-			expect(component).toHaveStyle(`gap:${space}`)
+			expect(component).toHaveStyle(`gap:${space}px`)
 		});
 	})
 });
 
-describe("Render Col Sizes", () => {
+describe("Render Space directions", () => {
 	const sizes = [...AxisArray, 'both']
 	sizes.forEach((size) => {
 		test(`render ${size} size`, () => {
-			const {container} = render(<Space direction={size as Axis}>Click Me</Space>);
+			const {container} = render(<Space gap={10} direction={size as Axis}>Click Me</Space>);
 			const component = container.firstChild;
-			expect(component).toHaveStyle(`gap:${size === 'horizontal'} ? '0 ${size}' : '${size} 0'`)
+			const gap = "10"
+			let style: string
+			if (size === 'horizontal') {
+				style = `gap: 0 ${10}px;`
+			} else if (size === 'vertical') {
+				style = `gap: ${10}px 0;`
+			} else {
+				style = `gap: ${gap}px;`
+			}
+			expect(component).toHaveStyle(style)
 		});
 	})
 });
