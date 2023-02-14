252b20db5e82b9b5d2dbf4510f703ccda5bd02e6
Daman <damankumar862@gmail.com>
2023-02-14 22:35:41 +0530

    minor changes

--- src/Dropdown/index.ts
+++ src/Dropdown/index.ts
@@ -4,6 +4,7 @@ import "../index.css"
 export * from './Dropdown';
 export * from './DropdownToggle';
 export * from './MenuItem';
+export * from './MenuHeading';
 export * from './DropdownMenu';
 export default Dropdown
 
