31e02cf63a9004682ef40f972a3846ff5f539d48
Daman <damankumar862@gmail.com>
2023-02-23 11:36:10 +0530

    chamges

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -4,5 +4,5 @@ import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react({jsxRuntime: 'classic'}), mdx()],
+	plugins: [react({jsxRuntime: 'react/jsx-runtime'}), mdx()],
 })
